640b02fd912cddb2f0af6f38527ccbbbe5aebdf6
Nidhi Dama <nidhidama25@gmail.com>
2024-04-28 10:46:38 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-function withOpacity(variableName){
-  return ({opacityValue})=>{
-    if(opacityValue!==undefined){
-      return `rgba(var(${variableName}), ${opacityValue})`
-    }else{
-      return `rgb(var(${variableName}))`
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    } else {
+      return `rgb(var(${variableName}))`;
     }
-  }
-
+  };
 }
 module.exports = {
   content: [
@@ -17,9 +16,9 @@ module.exports = {
     extend: {
       colors: {
         primarygray: "#f8f8f8",
-        qblack: 'rgb(var(--secondary-color))',
-        qyellow: 'rgb(var(--primary-color))',
-        qyellowlow: withOpacity('--primary-color'),
+        qblack: "rgb(var(--secondary-color))",
+        qyellow: "rgb(var(--primary-color))",
+        qyellowlow: withOpacity("--primary-color"),
         qred: "#EF262C",
         qgray: "#797979",
         qblacktext: "#1D1D1D",
@@ -40,5 +39,4 @@ module.exports = {
       space: ["responsive", "direction"],
     },
   },
-  plugins: [require("@tailwindcss/line-clamp")],
 };
