cc287031eab8dac197beb422ad3b7aa84044d1a4
Nidhi Dama <nidhidama25@gmail.com>
2024-05-09 07:05:55 +0530

    login

--- pages/api/upload-image.ts
+++ pages/api/upload-image.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
-import path from 'path';
 import { NextApiRequest, NextApiResponse } from 'next';
+import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
