bc2aba3f7af7f75e350231fbabb3b73bc356294a
Nidhi Dama <nidhidama25@gmail.com>
2024-06-08 13:30:26 +0530

    trending categories

--- pages/single-product/index.js
+++ pages/single-product/index.js
@@ -1,13 +1,9 @@
 import { useRouter } from "next/router";
 import PageHead from "../../src/components/Helpers/PageHead";
 import SingleProductPage from "../../src/components/SingleProductPage";
+
 const SingleProduct = ({ data }) => {
   const router = useRouter();
-  // useEffect(() => {
-  //   if (router && !router.query.slug) {
-  //     router.push("*");
-  //   }
-  // });
 
   return (
     <>
@@ -15,14 +11,15 @@ const SingleProduct = ({ data }) => {
         title={`${data.product && data.product.seo_title}`}
         metaDes={data.product && data.product.seo_description}
       />
-      {router.query.slug && <SingleProductPage details={data} />}
+      {router.query.id && <SingleProductPage details={data} />}
     </>
   );
 };
+
 export const getServerSideProps = async (context) => {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}api/product/${context.query.slug}`
+      `${process.env.NEXT_PUBLIC_BASE_URL}api/product/${context.query.id}`
     );
     const data = await res.json();
     console.log("Response from server:", data); // Log the response
