11b7ba21403f9345182fb8d1feeed41270d91178
Nidhi Dama <nidhidama25@gmail.com>
2024-06-08 09:08:27 +0530

    trending categories

--- src/components/Home/CategorySection.jsx
+++ src/components/Home/CategorySection.jsx
@@ -6,7 +6,7 @@ function CategorySection({ sectionTitle, categories }) {
   return (
     <div>
       <div data-aos="fade-up" className="category-section-wrapper w-full">
-        <div className="container-x mx-auto pb-[60px]  ">
+        <div className=" pb-[60px]  ">
           <div className="bg-[#FFF4D4] p-20 rounded-md w-full md:grid xl:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-[30px] md:flex md:flex-row w-70 h-70">
             {/* Add the following div for the Trending Category heading */}
             <div className="col-span-3 text-center  p-5">
@@ -79,7 +79,7 @@ function CategorySection({ sectionTitle, categories }) {
         </div>
       </div>
       <div data-aos="fade-up" className="category-section-wrapper w-full">
-        <div className="container-x mx-auto pb-[60px]">
+        <div className=" pb-[60px]">
           <div className="bg-[#F3C0C0] p-20 rounded-md w-full flex justify-center gap-[30px]">
             {categories && (
               <>
